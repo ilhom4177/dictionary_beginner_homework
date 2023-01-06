7e0bac3daec981653b44f64e295506f17a3df3c8
ilhom <ilhommamatqulov276@gmail.com>
2023-01-06 10:23:56 +0500

    find_max_key.py

--- find_max_key.py
+++ find_max_key.py
@@ -6,4 +6,9 @@ def find_max_key(data: dict):
     Returns:
         int: The maximum key in the dictionary.
     """
-    return 
+    max = 0
+    for i in data.keys():
+        if max < i:
+            max = i
+    return max
+print(find_max_key({1.4:'a', 7.8:'b', 4: 'c'})) 
