9d5313e7aa9d0740f6c51fc5e5feda4eb012d38b
ilhom <ilhommamatqulov276@gmail.com>
2023-01-07 11:42:19 +0500

    get_user_country.py

--- get_user_country.py
+++ get_user_country.py
@@ -8,4 +8,11 @@ def get_user_country(data:list, name:str) -> list:
     Returns:
         str: The country of the user with the given name
     """
-    return 
+    name1 = data[0]['name']
+    name2 = data[1]['name']
+    if name == name1:
+        return data[0]['country']
+    elif name == name2:
+        return data[1]['country']
+print(get_user_country([{'name': 'John', 'country': 'USA'}, {'name': 'Mary', 'country': 'UK'}],"John"))
+    
